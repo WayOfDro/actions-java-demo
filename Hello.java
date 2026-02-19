0f4fd5176518f5e171cbaa7818b54ae686d81562
Nathaniel Hackbardt <wayofdro@gmail.com>
2026-02-18 20:10:46 -0500

    fixed the code

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class Hello{
-  public static void main(args[]){
-    System.println{"hello from GitHub Actions");
-                  }
-  }
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello from GitHub Actions");
+    }
+}
