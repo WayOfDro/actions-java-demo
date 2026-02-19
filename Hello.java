7b52b61bbe6000f3670923847d60d8eafb59abaa
Nathaniel Hackbardt <wayofdro@gmail.com>
2026-02-18 20:13:00 -0500

    break the code more

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class Hello {
+public class Helloo {
     public static void main(String[] args) {
-        System.out.println("Hello from GitHub Actions");;
+        System.out.println("Hello from GitHub Actions");
     }
 }
